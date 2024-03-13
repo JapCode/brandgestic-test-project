0d7d2ba578ce41687914fd9cb8dff272ef97de80
JapCode <japcrowley.thetraveler@gmail.com>
2024-03-13 03:18:19 -0400

    end mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ function App() {
     <main className="relative flex min-h-screen flex-col bg-green  ">
       <NavBar navItems={query.data?.nav} />
       <div className="flex-1 flex-grow">
-        <MaxWidthWrapper className="h-80">
-          <div className="grid grid-cols-2 pt-[8%]">
-            <div>
+        <MaxWidthWrapper className="md:h-80 ">
+          <div className="grid md:grid-cols-2 pt-[8%] min-h-screen md:min-h-fit">
+            <div className="w-full flex items-center flex-col md:items-start">
               {query.isSuccess && (
                 <h1
-                  className="text-5xl font-workSansSemiBold font-semibold md:text-5xl 2xl:text-7xl max-w-[450px] 2xl:max-w-[560px] leading-snug 2xl:leading-tight text-softGreen"
+                  className="text-3xl text-center max-w-[250px] md:text-start font-workSans md:font-workSansSemiBold md:text-5xl 2xl:text-7xl md:max-w-[430px] 2xl:max-w-[560px] leading-snug 2xl:leading-tight text-softGreen"
                   dangerouslySetInnerHTML={{
                     __html: query.data?.home["header-title"],
                   }}
@@ -26,7 +26,7 @@ function App() {
               )}
 
               {/* call to action button */}
-              <button className="w-72 h-14 relative 2xl:w-[470px] 2xl:h-16 2xl:mt-10">
+              <button className="w-64 h-12 md:w-72 md:h-14 relative 2xl:w-[470px] 2xl:h-16 2xl:mt-10">
                 <div className="justify-center absolute items-center gap-3 2xl:gap-6 z-10 flex  bg-softGreen w-full h-full">
                   <div className="rounded-full bg-purple w-9 h-9 overflow-hidden">
                     <img
@@ -35,45 +35,70 @@ function App() {
                       className="w-full h-full object-contain object-center"
                     />
                   </div>
-                  <span className="text-xl font-workSansSemiBold 2xl:text-2xl  text-green">
+                  <span className="text-sm md:text-xl font-workSansSemiBold 2xl:text-2xl  text-green">
                     {query.data?.home["header-cta"]}
                   </span>
                 </div>
-                <div className="w-full h-full bg-purple absolute -right-2 top-10 2xl:top-12 2xl:-right-4"></div>
+                <div className="w-full h-full bg-purple absolute -right-1.5 top-8 md:-right-2 md:top-10 2xl:top-12 2xl:-right-4"></div>
               </button>
             </div>
 
-            {/* background right element */}
-            <div className="hidden md:flex flex-col justify-end items-center bg-purple absolute right-0 top-0 w-2/4 h-full purpleBgClip"></div>
+            <div className="mt-20 md:mt-0">
+              {/* ticket */}
+              <div className="flex relative w-full md:absolute md:right-[32%] md:top-[12%] md:w-[340px] 2xl:w-[415px] 2xl:h-[190px] 2xl:top-[13%] h-[150px] z-10 md:ticketBgClip bg-white">
+                <div className="h-full w-10 bg-softYellow flex items-center justify-center 2xl:w-14">
+                  <p className="transform -rotate-90 w-fit font-workSansSemiBold text-lg text-green 2xl:text-xl">
+                    COVERED
+                  </p>
+                </div>
+                <div className="w-12 h-12 2xl:h-14 2xl:w-14 flex items-center justify-center bg-softGreen rounded-full absolute left-[21%] top-[15%] md:left-[25%] md:top-[16%]">
+                  <span className="font-workSansMedium text-lg 2xl:text-xl text-green">
+                    $
+                  </span>
+                </div>
+                <div className="w-full h-full justify-center flex-col absolute left-[9%] top-[8%] 2xl:top-[8%] 2xl:left-[10%] items-center flex -ml-8">
+                  <span className="font-workSansExtraBold text-5xl text-green 2xl:text-7xl">
+                    500k
+                  </span>
+                  <span className="font-workSansMedium text-lg text-green 2xl:text-2xl">
+                    $10.99 / month
+                  </span>
+                </div>
+              </div>
 
-            {/* image right element */}
+              <div className="w-full h-[230px] bg-softYellow md:hidden">
+                <div className="w-full flex flex-col justify-center items-center">
+                  <div className="w-1 h-10 bg-purple"></div>
+                  <SvgArrow />
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-full pt-4 gap-4">
+                  <h3 className="text-green font-inter font-bold text-xl">
+                    TechCrunch
+                  </h3>
+                  <h3 className="text-green font-dmSerif text-2xl ">Forbes</h3>
+                  <h3 className="text-green font-dmSerif text-xl ">
+                    THE WALL STREET JOURNAL
+                  </h3>
+                </div>
+              </div>
+            </div>
+
+            {/* background right element */}
+            <div className="w-full h-72 md:flex flex-col justify-end items-center bg-purple md:absolute md:right-0 md:top-0 md:w-2/4 md:h-full md:purpleBgClip">
+              <img
+                src={headerImage}
+                alt="header image"
+                className="md:hidden w-full h-full object-cover object-center"
+              />
+            </div>
+
+            {/* image right element desktop */}
             <img
               src={headerImage}
               alt="header image"
               className="hidden md:block h-[85%] w-[50%] object-cover absolute right-0 bottom-0 object-center z-20"
             />
-
-            {/* ticket */}
-            <div className="hidden md:flex absolute right-[32%] top-[12%] w-[340px] 2xl:w-[415px] 2xl:h-[190px] 2xl:top-[13%] h-[150px] z-10 ticketBgClip bg-white">
-              <div className="h-full w-10 bg-softYellow flex items-center justify-center 2xl:w-14">
-                <p className="transform -rotate-90 w-fit font-workSansSemiBold text-lg text-green 2xl:text-xl">
-                  COVERED
-                </p>
-              </div>
-              <div className="w-12 h-12 2xl:h-14 2xl:w-14 flex items-center justify-center bg-softGreen rounded-full absolute left-[25%] top-[16%]">
-                <span className="font-workSansMedium text-lg 2xl:text-xl text-green">
-                  $
-                </span>
-              </div>
-              <div className="w-full h-full justify-center flex-col absolute left-[9%] top-[8%] 2xl:top-[8%] 2xl:left-[10%] items-center flex -ml-8">
-                <span className="font-workSansExtraBold text-5xl text-green 2xl:text-7xl">
-                  500k
-                </span>
-                <span className="font-workSansMedium text-lg text-green 2xl:text-2xl">
-                  $10.99 / month
-                </span>
-              </div>
-            </div>
 
             {/* bottomElement */}
             <div className="hidden md:block w-[55.5%] h-[340px] 2xl:h-[400px] z-30  bg-softYellow bottomBgClip absolute bottom-0 left-0">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,10 +10,10 @@ const NavBar = (props: INavBarProps) => {
   const { navItems } = props;
 
   return (
-    <nav className="bg-transparent sticky z-50 top-0 inset-x-0 h-16 pt-4 2xl:pt-6">
+    <nav className="bg-green md:bg-transparent sticky z-50 top-0 inset-x-0 h-16 pt-4 2xl:pt-6">
       <MaxWidthWrapper>
         {navItems && navItems.length > 0 && (
-          <div className="flex justify-between">
+          <div className="flex justify-between px-4">
             <ul className="flex justify-between w-[600px] items-center">
               <li>
                 <img src={logo} alt="logo" className="h-8 " />
@@ -26,8 +26,23 @@ const NavBar = (props: INavBarProps) => {
                   {item.title}
                 </li>
               ))}
+              <button className="md:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="none"
+                >
+                  <path
+                    fill="#fff"
+                    fill-rule="evenodd"
+                    d="M4.5 7.5A1.5 1.5 0 0 1 6 6h18a1.5 1.5 0 0 1 0 3H6a1.5 1.5 0 0 1-1.5-1.5Zm0 7.5A1.5 1.5 0 0 1 6 13.5h18a1.5 1.5 0 1 1 0 3H6A1.5 1.5 0 0 1 4.5 15Zm9 7.5A1.5 1.5 0 0 1 15 21h9a1.5 1.5 0 1 1 0 3h-9a1.5 1.5 0 0 1-1.5-1.5Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
             </ul>
-            <ul className="hidden md:flex justify-between w-[250px] items-center">
+            <ul className="hidden  md:flex justify-between w-[250px] items-center">
               <li className="text-white text-md font-workSans font-bold">
                 Log in
               </li>
